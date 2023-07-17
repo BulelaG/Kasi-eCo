65b1f8c68ab36cdfe033a23dbfcee98ef8d69615
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-07-17 13:11:49 +0200

    updating the limit of a payload

--- Backend/index.js
+++ Backend/index.js
@@ -4,10 +4,10 @@ const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.use(express.json());
+// app.use(express.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json({limit:'4mb'}));
+app.use(bodyParser.urlencoded({extended:true, limit: "50mb"}));
+app.use(bodyParser.json({limit:'50mb'}));
 
 // MY ROUTES
 const userRoutes = require("./routes/user.routes")

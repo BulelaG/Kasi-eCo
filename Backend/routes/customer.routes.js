1772fbc2759d2e84fe35b22a812125ff76e7d9b3
Bulela Gomoshe <bulelagomoshe1@gmail.com>
2023-07-17 12:56:20 +0200

    updating headers

--- Backend/routes/customer.routes.js
+++ Backend/routes/customer.routes.js
@@ -3,6 +3,13 @@ module.exports = rs => {
     const controller = require("../controller/customer.controller")
 
     // Customer endpoints
+    app.use((req, res, next) => {
+      res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+      );
+      next();
+    })
 
    router.post('/customer-signup', controller.createCustomer); // ADD CUSTOMER
 
--- Backend/routes/products.routes.js
+++ Backend/routes/products.routes.js
@@ -1,7 +1,15 @@
-module.exports = rs => {
+module.exports = app => {
     const router = require("express").Router();
     const controller = require("../controller/products.controller")
     const { authJwt } = require("../middlewares")
+
+  	app.use((req, res, next) => {
+      res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+      );
+      next();
+    })
 
  // Products endpoints
 //  [ authJwt.verifyToken ],
@@ -19,5 +27,5 @@ module.exports = rs => {
     
    router.delete('/:id', controller.deleteProduct); // DELETE 1 PRODUCT
   
-  rs.use('/v1/products/',router)  
+  app.use('/v1/products/',router)  
 }
